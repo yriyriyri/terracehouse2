265d2f44cf0b1172c76fcede23ed394c70b9409b
yriyriyri <oscar.paterson93@gmail.com>
2026-02-05 22:01:03 +0900

    audio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import StreamGrid from "@/components/StreamGrid";
 import ChatPanel from "@/components/ChatPanel";
 
+const MIC_HLS = "https://streams.towergroup.tv/macmini-mic/index.m3u8";
+
 export default function Page() {
   return (
     <>
@@ -14,6 +16,16 @@ export default function Page() {
       >
         <source src="/bg.mp4" type="video/mp4" />
       </video>
+
+      <audio
+        autoPlay
+        controls
+        playsInline
+        preload="none"
+        crossOrigin="anonymous"
+      >
+        <source src={MIC_HLS} type="application/vnd.apple.mpegurl" />
+      </audio>
 
       <main className="page">
         <section className="mainRow">
