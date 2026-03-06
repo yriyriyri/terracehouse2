6653c2228c04c6c303f04b59636384dafab268e2
yriyriyri <oscar.paterson93@gmail.com>
2026-03-06 17:42:02 +0900

    3 new cams

--- src/components/StreamGrid.tsx
+++ src/components/StreamGrid.tsx
@@ -6,6 +6,33 @@ import { useMemo } from "react";
 const STATUS_TOKEN_DEFAULT =
   "bc1qc09c2tsyw9rj5ema9rsvk3e7acr8dasagn8q0d";
 
+  function EmptyTile() {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          backdropFilter: "blur(1px)",
+          WebkitBackdropFilter: "blur(1px)",
+          border: "1px solid rgba(255,255,255,0.08)",
+        }}
+      >
+        <span
+          style={{
+            fontSize: "13px",
+            color: "rgba(255, 255, 255, 0.8)",
+          }}
+        >
+          Donate for more cameras!
+        </span>
+      </div>
+    );
+  }
+
 export default function StreamGrid() {
   const streams = useMemo(
     () => [
@@ -29,17 +56,40 @@ export default function StreamGrid() {
         label: "Living Room",
         src: "https://streams.towergroup.tv/living-room/index.m3u8",
       },
+      {
+        key: "upstairs",
+        label: "Upstairs",
+        src: "https://streams.towergroup.tv/upstairs/index.m3u8",
+      },
+      {
+        key: "downstairs",
+        label: "Downstairs",
+        src: "https://streams.towergroup.tv/downstairs/index.m3u8",
+      },
+      {
+        key: "tv",
+        label: "TV",
+        src: "https://streams.towergroup.tv/tv/index.m3u8",
+      },
+      null,
+      null,
     ],
     []
   );
 
   return (
-    <div style={{ alignSelf: "start", display: "inline-block", width: "clamp(100%, 55vw, 600px)",}}>
+    <div
+      style={{
+        alignSelf: "start",
+        display: "inline-block",
+        width: "clamp(100%, 55vw, 600px)",
+      }}
+    >
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "repeat(2, 1fr)",
-          gridTemplateRows: "repeat(2, 1fr)",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridTemplateRows: "repeat(3, 1fr)",
           gap: 4,
           padding: 4,
           aspectRatio: "4 / 3",
@@ -50,11 +100,15 @@ export default function StreamGrid() {
           border: "1px solid rgba(255,255,255,0.08)",
         }}
       >
-        {streams.map((s) => (
-          <HlsTile key={s.key} src={s.src} />
-        ))}
-      </div>  
-    <div className="statusBar">{STATUS_TOKEN_DEFAULT}</div>
+        {streams.map((s, i) =>
+          s ? (
+            <HlsTile key={s.key} src={s.src} />
+          ) : (
+            <EmptyTile key={`empty-${i}`} />
+          )
+        )}
+      </div>
+      <div className="statusBar">{STATUS_TOKEN_DEFAULT}</div>
     </div>
   );
 }
